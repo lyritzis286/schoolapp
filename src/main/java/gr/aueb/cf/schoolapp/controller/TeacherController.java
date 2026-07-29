878a2403cf0343ddeb386fe75d92d24cbbe5bfba
lyritzis286 <lyritzis286@gmail.com>
2026-07-29 16:48:52 +0300

    Add teacher insertion logic and validation in TeacherController

--- src/main/java/gr/aueb/cf/schoolapp/controller/TeacherController.java
+++ src/main/java/gr/aueb/cf/schoolapp/controller/TeacherController.java
@@ -1,8 +1,14 @@
 package gr.aueb.cf.schoolapp.controller;
-
+import gr.aueb.cf.schoolapp.core.exceptions.EntityAlreadyExistsException;
+import gr.aueb.cf.schoolapp.core.exceptions.EntityInvalidArgumentException;
+import gr.aueb.cf.schoolapp.core.exceptions.EntityNotFoundException;
 import gr.aueb.cf.schoolapp.dto.RegionReadOnlyDTO;
 import gr.aueb.cf.schoolapp.dto.TeacherInsertDTO;
+import gr.aueb.cf.schoolapp.dto.TeacherReadOnlyDTO;
 import gr.aueb.cf.schoolapp.model.Teacher;
+import gr.aueb.cf.schoolapp.service.IRegionService;
+import gr.aueb.cf.schoolapp.service.ITeacherService;
+import gr.aueb.cf.schoolapp.validator.TeacherInsertValidator;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -21,8 +27,9 @@ import java.util.List;
 @RequiredArgsConstructor
 public class TeacherController {
 
-//    private final ITeacherService teacherService;
-//    private final IRegionService regionService;
+    private final ITeacherService teacherService;
+    private final IRegionService regionService;
+    private final TeacherInsertValidator teacherInsertValidator;
 
     @GetMapping("/insert")
     public String getTeacherForm(Model model) {
@@ -38,6 +45,29 @@ public class TeacherController {
                                 BindingResult bindingResult, Model model,
                                 RedirectAttributes redirectAttributes) {
 
+        teacherInsertValidator.validate(teacherInsertDTO, bindingResult);
+        if (bindingResult.hasErrors()) {
+//            model.addAttribute("regionsReadOnlyDTO", regions());
+            return "teacher-insert";
+        }
+        try {
+            TeacherReadOnlyDTO teacherReadOnlyDTO = teacherService.saveTeacher(teacherInsertDTO);
+            redirectAttributes.addAttribute("teacher", teacherReadOnlyDTO);
+            return "redirect:/teachers/success";    //controller
+        }catch (EntityAlreadyExistsException | EntityInvalidArgumentException e){
+            model.addAttribute("errorMessage", e.getMessage());
+            return "teacher-insert";
+        }
+
+
+    }
+
+    @GetMapping("/success")
+    public String teacherInsertSuccess(Model model) {
+        if(!model.containsAttribute("teacherInsertDTO")) {
+            return "redirect:/teachers/success";
+        }
+        return "teacher-success";
     }
 
     @ModelAttribute("regionsReadOnlyDTO")
