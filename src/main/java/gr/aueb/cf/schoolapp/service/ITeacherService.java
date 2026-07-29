e97177973daa1323a456f6a3e625755e3d6ad3ae
lyritzis286 <lyritzis286@gmail.com>
2026-07-29 15:00:25 +0300

    Add ITeacherService interface and TeacherRepository for teacher data management

---
+++ src/main/java/gr/aueb/cf/schoolapp/service/ITeacherService.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.schoolapp.service;
+
+import gr.aueb.cf.schoolapp.core.exceptions.EntityAlreadyExistsException;
+import gr.aueb.cf.schoolapp.core.exceptions.EntityInvalidArgumentException;
+import gr.aueb.cf.schoolapp.core.exceptions.EntityNotFoundException;
+import gr.aueb.cf.schoolapp.dto.TeacherInsertDTO;
+import gr.aueb.cf.schoolapp.dto.TeacherReadOnlyDTO;
+
+public interface ITeacherService {
+
+    TeacherReadOnlyDTO saveTeacher(TeacherInsertDTO teacherInsertDTO)
+        throws EntityInvalidArgumentException, EntityAlreadyExistsException;
+}
