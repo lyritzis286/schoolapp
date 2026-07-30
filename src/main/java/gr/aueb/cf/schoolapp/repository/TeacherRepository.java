9209fe2ba4180cb41a81fd54ba79e68c697a42e8
lyritzis286 <lyritzis286@gmail.com>
2026-07-30 14:54:34 +0300

    Add pagination and entity graph support for non-deleted teachers in TeacherRepository

--- src/main/java/gr/aueb/cf/schoolapp/repository/TeacherRepository.java
+++ src/main/java/gr/aueb/cf/schoolapp/repository/TeacherRepository.java
@@ -1,6 +1,9 @@
 package gr.aueb.cf.schoolapp.repository;
 
 import gr.aueb.cf.schoolapp.model.Teacher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -10,4 +13,10 @@ public interface TeacherRepository  extends JpaRepository<Teacher, Long> {
 
     Optional<Teacher> findByVat(String vat);
     Optional<Teacher> findByUuid(UUID uuid);
+
+    @EntityGraph(attributePaths = {"region"})
+    Page<Teacher> findAllByDeletedFalse(Pageable pageable);
+
+    Optional<Teacher> findByVatAndDeletedFalse(String vat);
+    Optional<Teacher> findByUuidAndDeletedFalse(UUID uuid);
 }
